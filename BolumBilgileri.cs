484918a9a9ccf40e3cfa9a2ed61211c3e75fbeec
ersinelbir <34042067+ersinelbir@users.noreply.github.com>
2017-12-28 21:48:07 +0300

    Add files via upload

---
+++ BolumBilgileri.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MznTkp
+{
+    public partial class BolumBilgileri : Form
+    {
+        public BolumBilgileri()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-GNK4VP1;Initial Catalog=Giris;Integrated Security=True");
+        private void goster2()
+        {
+            string komut2 = "select *from BolumBilgileri";
+            SqlDataAdapter da2 = new SqlDataAdapter(komut2, baglanti);
+            DataTable tablo2 = new DataTable();
+            da2.Fill(tablo2);
+            dataGridView2.DataSource = tablo2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Programı Kapatmak İstediğinize Eminmisiniz ?", "DİKKAT", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Hide();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MezunTakip mt = new MezunTakip();
+            mt.Show();
+            this.Hide();
+        }
+
+        private void BolumBilgileri_Load(object sender, EventArgs e)
+        {
+            goster2();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Giris.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MznTkp
+{
+    public partial class Giris : Form
+    {
+        public Giris()
+        {
+            InitializeComponent();
+            textBox2.PasswordChar = '*';
+            textBox2.MaxLength = 15;
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-GNK4VP1;Initial Catalog=Giris;Integrated Security=True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select *from Giris where kullaniciadi='" + textBox1.Text + "' AND sifre='" + textBox2.Text + "'", baglanti);
+
+            SqlDataReader dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+                MezunTakip mt = new MezunTakip();
+                mt.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı ya da şifre yanlış");
+            }
+
+            baglanti.Close();
+        }
+    }
+}
